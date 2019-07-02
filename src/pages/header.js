bd7ce4a21576ba9c62acc4e5220d3f52fb681741
Rudrasagar <rudrasagar.tn@gmail.com>
2019-07-02 17:41:29 +0530

    Added Logout button and placeholder for loggedin username

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 
-function Header(props){
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-primary-navbar-custom">
-          <div className="container-fluid">
-            <div className="nav-item" href="#">
-              <h3>Sowing App</h3>
-            </div>
-          </div>
-        </nav>
-    )
+function Header(props) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary-navbar-custom">
+      <div className="container-fluid">
+        <div className="nav-item" href="#">
+          <h3>Sowing App</h3>
+        </div>
+        <span class="d-block p-2 text-white">
+          Username,
+          <button class="btn btn-outline-info text-white" type="submit">
+            Logout
+          </button>
+        </span>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
